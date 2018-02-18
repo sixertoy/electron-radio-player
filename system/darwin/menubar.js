0356259a88723697762e2e083299cfe7658e6ae9
sixertoy <sixertoy.github@gmail.com>
2018-02-18 09:41:10 +0100

    🍪  update main renderer edit menu

--- system/darwin/menubar.js
+++ system/darwin/menubar.js
@@ -3,14 +3,14 @@ const { fp } = require('./../utils');
 
 // application
 const view = require('./menu/view');
-const edit = require('./menu/edit');
 const help = require('./menu/help');
 const about = require('./menu/about');
+const edit = require('./../menu/edit');
 const fenetre = require('./menu/fenetre');
 
 const { Menu } = electron;
 module.exports = (win) => {
-  const template = fp.each([win, 'toto'], about, edit, view, fenetre, help);
+  const template = fp.each([win], about, edit, view, fenetre, help);
   const menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
   return win;
