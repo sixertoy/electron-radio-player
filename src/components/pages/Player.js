2b2274f2e3eff08db4dd885bb3b2076710e4c241
sixertoy <sixertoy.github@gmail.com>
2018-02-22 17:55:44 +0100

    🗑  update

--- src/components/pages/Player.js
+++ src/components/pages/Player.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 // application
+import { slugify } from './../../lib/slugify';
 import MenuBar from './../MenuBar';
 import Podcasts from './../Podcasts';
 import Playlist from './../Playlist';
@@ -8,26 +11,61 @@ import StationForm from './../StationForm';
 import AudioPlayer from './../AudioPlayer';
 import SearchResults from './../SearchResults';
 
-const MainWindow = () => (
-  <div id="page-player"
-    className="app-page flex-rows">
-    <AudioPlayer />
-    <div className="page-screens-container">
-      <div id="playlist-screen" className="page-screen">
-        <Playlist />
-      </div>
-      <div id="podcasts-screen" className="page-screen">
-        <Podcasts />
-      </div>
-      <MenuBar />
-      <div id="searchresults-screen" className="page-screen">
-        <SearchResults />
-      </div>
-      <div id="create-screen" className="page-screen">
-        <StationForm />
-      </div>
-    </div>
-  </div>
-);
+class PlayerWindow extends React.PureComponent {
 
-export default MainWindow;
+  constructor (props) {
+    super(props);
+    const { screenslug } = props;
+    this.state = { screens: [], screenslug };
+  }
+
+  componentWillReceiveProps ({ screenslug }) {
+    if (screenslug !== this.state.screenslug) {
+      this.setState(() => ({
+        screens: [].concat([
+          screenslug === 'screen-podcasts'
+            && { id: 'podcasts-screen', component: <Podcasts /> },
+          screenslug === 'screen-searchresults'
+            && { id: 'searchresults-screen', component: <SearchResults /> },
+          screenslug === 'screen-create'
+            && { id: 'create-screen', component: <StationForm /> },
+        ]).filter(val => val),
+      }));
+    }
+  }
+
+  render () {
+    const { screens } = this.state;
+    return (
+      <div id="page-player"
+        className="app-page flex-rows">
+        <AudioPlayer />
+        <div className="page-screens-container">
+          <div id="playlist-screen" className="page-screen">
+            <Playlist />
+          </div>
+          {screens && screens.map(obj =>
+            (<div id={obj.id}
+              key={obj.id}
+              className="page-screen">{obj.component}</div>))}
+          <MenuBar />
+        </div>
+      </div>
+    );
+  }
+}
+
+PlayerWindow.propTypes = {
+  screenslug: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  let { pathname } = state.router.location;
+  pathname = slugify(pathname || 'player');
+  pathname = pathname.split('-');
+  return ({
+    screenslug: (pathname[1] ? `screen-${pathname[1]}` : ''),
+  });
+};
+
+export default connect(mapStateToProps)(PlayerWindow);
