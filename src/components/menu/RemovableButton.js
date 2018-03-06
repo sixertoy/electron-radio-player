8d423b589197b8b4842757c6a1173bc48ff87509
sixertoy <sixertoy.github@gmail.com>
2018-03-06 10:03:44 +0100

    💅  replace removable trash icon by list

--- src/components/menu/RemovableButton.js
+++ src/components/menu/RemovableButton.js
@@ -3,22 +3,26 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 // application
+import { noop } from './../../lib/noop';
 import { toggleRemovable } from './../../actions';
 
-const RemovebleButton = ({ removable, dispatch }) => (
-  <button onClick={() => dispatch(toggleRemovable())}
-    className={`button removable ${removable ? 'active' : ''}`}>
-    <i className="icon icon-trash" />
+const RemovebleButton = ({ editable, dispatch, disabled }) => (
+  <button disabled={disabled}
+    onClick={disabled ? noop : () => dispatch(toggleRemovable())}
+    className={`button removable ${editable ? 'active' : ''}`}>
+    <i className="icon icon-list" />
   </button>
 );
 
 RemovebleButton.propTypes = {
   dispatch: PropTypes.func.isRequired,
-  removable: PropTypes.bool.isRequired,
+  disabled: PropTypes.bool.isRequired,
+  editable: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = state => ({
-  removable: state.removable,
+  editable: state.removable,
+  disabled: state.playlist.length <= 0,
 });
 
 export default connect(mapStateToProps)(RemovebleButton);
